352e15fe1edace0fc61babb6770b6253667e67e6
Xeno <53803953+blackice36@users.noreply.github.com>
2021-02-07 15:19:53 -0600

    Added Gaurd  Start Error Checking

--- Necromancy.Server/Model/CharacterModel/Character.cs
+++ Necromancy.Server/Model/CharacterModel/Character.cs
@@ -51,6 +51,7 @@ namespace Necromancy.Server.Model
         public BaseStat Hp;
         public BaseStat Mp;
         public BaseStat Od;
+        public BaseStat Gp;
 
         //Model
         public int activeModel;
@@ -145,6 +146,7 @@ namespace Necromancy.Server.Model
             Hp = new BaseStat(1000, 1000);
             Mp = new BaseStat(450, 500);
             Od = new BaseStat(150, 200);
+            Gp = new BaseStat(50, 50);
             takeover = false;
             skillStartCast = 0;
             battleAnim = 0;
