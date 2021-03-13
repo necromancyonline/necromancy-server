2566658c6fe7f8fde17be401dcf0f6b559599e80
Your Name <adam.shehadeh@yahoo.com>
2021-03-12 19:04:50 -0800

    Update send_shop_close.cs

--- Necromancy.Server/Packet/Area/send_shop_close.cs
+++ Necromancy.Server/Packet/Area/send_shop_close.cs
@@ -19,7 +19,7 @@ namespace Necromancy.Server.Packet.Area
         {
             IBuffer res = BufferProvider.Provide();
             res.WriteInt32(0);
-            Router.Send(client.Map, (ushort) AreaPacketId.recv_shop_close_r, res, ServerType.Area);
+            Router.Send(client, (ushort) AreaPacketId.recv_shop_close_r, res, ServerType.Area);
             SendShopNotifyClose(client);
         }
 
