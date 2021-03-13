2c6049424c4873d79122aad53215b354f082b0e8
Your Name <adam.shehadeh@yahoo.com>
2021-03-13 15:33:59 -0800

    Update RecvAuctionNotifyOpen.cs
    regather of struct

--- Necromancy.Server/Packet/Receive/Area/RecvAuctionNotifyOpen.cs
+++ Necromancy.Server/Packet/Receive/Area/RecvAuctionNotifyOpen.cs
@@ -15,54 +15,95 @@ namespace Necromancy.Server.Packet.Receive.Area
         protected override IBuffer ToBuffer()
         {
             IBuffer res = BufferProvider.Provide();
-            res.WriteInt32(5); // must be <= 5
+            int numEntries = 0xF;
+            res.WriteInt32(numEntries); //Less than or equal to 0xF
 
-            int numEntries = 0x5;
             for (int i = 0; i < numEntries; i++)
             {
-
-                res.WriteByte(1);
-                // res.WriteInt64(0xAAAAAAAA); // this is shown in the structure but for some reason isnt read
+                res.WriteByte(0);
 
                 res.WriteInt32(0);
                 res.WriteInt64(0);
-                res.WriteInt32(0);
-                res.WriteInt32(0);
+                res.WriteInt64(0);
+                res.WriteInt64(0);
                 res.WriteFixedString("soulname", 49);
                 res.WriteByte(1);
                 res.WriteFixedString("ToBeFound", 385);
                 res.WriteInt16(0);
                 res.WriteInt32(0);
 
+                res.WriteInt64(0);
                 res.WriteInt32(0);
-                res.WriteInt32(0);
-
             }
 
-            res.WriteInt32(8); // must be< = 8
+            numEntries = 0xE;
+            res.WriteInt32(numEntries); //Less than or equal to 0xE
 
-            int numEntries2 = 0x8;
-            for (int i = 0; i < numEntries2; i++)
+            for (int i = 0; i < numEntries; i++)
             {
                 res.WriteByte(0);
-                // res.WriteInt64(0xAAAAAAAA); // this is shown in the structure but for some reason isnt read
 
                 res.WriteInt32(0);
                 res.WriteInt64(0);
-                res.WriteInt32(0);
-                res.WriteInt32(0);
+                res.WriteInt64(0);
+                res.WriteInt64(0);
                 res.WriteFixedString("soulname", 49);
-                res.WriteByte(0);
+                res.WriteByte(1);
                 res.WriteFixedString("ToBeFound", 385);
                 res.WriteInt16(0);
                 res.WriteInt32(0);
 
+                res.WriteInt64(0);
                 res.WriteInt32(0);
-                res.WriteInt32(0);
-
             }
 
-            res.WriteByte(0); // bool
+            numEntries = 0x8;
+            res.WriteInt32(numEntries); //Less than or equal to 0x8
+
+            for (int i = 0; i < numEntries; i++)
+            {
+                res.WriteFixedString("fs0x49", 0x49);
+                res.WriteByte(0);
+                res.WriteByte(0);
+                res.WriteByte(0);
+                res.WriteByte(0);
+                res.WriteInt32(0);
+                res.WriteInt16(0);
+                res.WriteInt64(0);
+                res.WriteByte(0);
+                res.WriteByte(0); //Bool
+
+                res.WriteByte(0);//These are 3 separate bytes or a fixed string of 3 characters.
+                res.WriteByte(0);//
+                res.WriteByte(0);//
+
+                res.WriteInt64(0);
+                res.WriteInt64(0);
+                res.WriteFixedString("fs0xC1", 0xC1);//Fixed string of 0xC1 or 0xC1 bytes.
+                res.WriteByte(0);
+                res.WriteByte(0);
+            }
+
+            numEntries = 0x8;
+            res.WriteInt32(numEntries); //Less than or equal to 0x8
+
+            for (int i = 0; i < numEntries; i++)
+            {
+                res.WriteFixedString("fs0x49V2", 0x49);
+                res.WriteByte(0);
+                res.WriteByte(0);
+                res.WriteByte(0);
+                res.WriteByte(0);
+                res.WriteInt64(0);
+                res.WriteByte(0);
+                res.WriteInt64(0);
+                res.WriteInt64(0);
+                res.WriteFixedString("fs0xC1V2", 0xC1);//Fixed string of 0xC1 or 0xC1 bytes.
+                res.WriteByte(0);
+            }
+
+            res.WriteInt32(0);
+            res.WriteInt32(0);
             return res;
         }
     }
