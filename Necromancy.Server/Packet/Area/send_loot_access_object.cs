eba6cdb26e39eb7a996970529bebd500a086ba45
Xeno <53803953+blackice36@users.noreply.github.com>
2021-03-26 23:49:31 -0500

    Fix inventory overflow from loot and Item Guy
    Fix index out of bounds exceptions on errant DB entries

--- Necromancy.Server/Packet/Area/send_loot_access_object.cs
+++ Necromancy.Server/Packet/Area/send_loot_access_object.cs
@@ -31,10 +31,11 @@ namespace Necromancy.Server.Packet.Area
             int instanceID = packet.Data.ReadInt32();
             MonsterSpawn monster = client.Map.GetMonsterByInstanceId((uint) instanceID);
             Logger.Debug($"{client.Character.Name} is trying to loot object {instanceID}.  Inventory Space {client.Character.ItemManager.GetTotalFreeSpace(ItemZoneType.AdventureBag)}");
+            ItemLocation nextOpenLocation = client.Character.ItemManager.NextOpenSlot(ItemZoneType.AdventureBag);
 
             if (monster == null) result = -10;
             else if (monster.loot.ItemCountRNG == 0) result = -1;
-            else if (client.Character.ItemManager.GetTotalFreeSpace(ItemZoneType.AdventureBag) < 2) result = -207; //expand to all inventory. TODO            
+            else if (nextOpenLocation.ZoneType == ItemZoneType.InvalidZone) result = -207; //expand to all inventory. TODO            
 
             IBuffer res2 = BufferProvider.Provide();
             res2.WriteInt32(result);
