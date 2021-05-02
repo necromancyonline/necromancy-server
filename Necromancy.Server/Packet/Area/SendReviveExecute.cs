1b5d7aebf2007d9e8869d1c1880bd997cb9de1c0
Unknownone69 <adam.shehadeh@yahoo.com>
2021-05-02 07:16:50 -0700

    Update SendReviveExecute.cs

--- Necromancy.Server/Packet/Area/SendReviveExecute.cs
+++ Necromancy.Server/Packet/Area/SendReviveExecute.cs
@@ -1,5 +1,7 @@
 using Arrowgene.Buffers;
+using Arrowgene.Logging;
 using Necromancy.Server.Common;
+using Necromancy.Server.Logging;
 using Necromancy.Server.Model;
 using Necromancy.Server.Packet.Id;
 using Necromancy.Server.Packet.Receive.Area;
@@ -8,6 +10,7 @@ namespace Necromancy.Server.Packet.Area
 {
     public class SendReviveExecute : ClientHandler
     {
+        private static readonly NecLogger _Logger = LogProvider.Logger<NecLogger>(typeof(SendReviveExecute));
         public SendReviveExecute(NecServer server) : base(server)
         {
         }
@@ -21,6 +24,15 @@ namespace Necromancy.Server.Packet.Area
             router.Send(reviveExecute, client);
 
             client.character.hp.SetCurrent(client.character.hp.max);
+            client.character.deadType = 0; //Is this needed?
+            if (!server.database.UpdateCharacter(client.character)) _Logger.Error("Could not update character after revive.");
+
+            if (client.map != null && client.map.deadBodies.ContainsKey(client.character.deadBodyInstanceId))//Don't have access to last map on a reset of client, this fails.
+            {
+                RecvObjectDisappearNotify recvObjectDisappearNotify = new RecvObjectDisappearNotify(client.character.deadBodyInstanceId);
+                router.Send(client.map, recvObjectDisappearNotify.ToPacket(), client);
+                client.map.deadBodies.Remove(client.character.deadBodyInstanceId);
+            }
         }
     }
 }
